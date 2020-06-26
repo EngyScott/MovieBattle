83b6b0bdc39e88715ee23dfd656d918d18b6d66a
EngyScott <engy.scott@ymail.com>
2020-06-26 09:28:42 +0200

    non-refactored

--- index.js
+++ index.js
@@ -31,7 +31,10 @@ const onInput = async (event)=>{
 
     const movies = await fetchData(event.target.value);
     resultsWrapper.innerHTML = "";
-    dropdown.classList.add('is-active');
+
+    if(movies.length){
+        dropdown.classList.add('is-active');
+    }
 
     for(let movie of movies){
         const option = document.createElement('a');
@@ -43,12 +46,51 @@ const onInput = async (event)=>{
             ${movie.Title}
         `;
         resultsWrapper.appendChild(option);
+
+        option.addEventListener('click', ()=>{
+            input.value = movie.Title;
+            dropdown.classList.remove('is-active');
+            movieSelect(movie);
+        })
+        
     }
 }
 input.addEventListener('input', debounce(onInput, 500))
 
 document.addEventListener('click', event =>{
     if(!root.contains(event.target)){
-        dropdown.classList.remove('is-acive')
+        dropdown.classList.remove('is-active')
     }
 })
+
+const movieSelect = async (movie) => {
+    const response = await axios.get('http://www.omdbapi.com/',{
+        params: {
+            apikey:'458aad73',
+            i: movie.imdbID
+        }
+    })
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+
+}
+
+const movieTemplate = (movieDetail)=>{
+    return `
+        <article class="media">
+            <figure class="media-left">
+                <p class="image">
+                    <img src="${movieDetail.Poster}"/>
+                </p>
+            </figure>
+
+            <div class="media-content">
+                <div class="content">
+                    <h1>${movieDetail.Title}</h1>
+                    <h4>${movieDetail.Genre}</h4>
+                    <p>${movieDetail.Plot}</p>
+
+                </div>
+            </div>
+        <article>
+    `
+}
